c1d9beb1e9b75cd597021032e727ac9501248177
susan-t <susan.thao@outlook.com>
2026-03-07 13:53:14 -0500

    basic image structure of website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+
+import Home from "./Home";
+import About from "./About";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return(
+    <Router>
+        <Routes>
+            <Route
+                path="/home"
+                element={<Home />}
+            />
+            <Route
+                path="/about"
+                element={<About />}
+            />
+            {/* <Route
+                path="/contactus"
+                element={<ContactUs />}
+            />  */}
+            <Route
+                path="*"
+                element={<Navigate to="/home" />}
+            />
+        </Routes>
+    </Router>
   );
+  
 }
 
 export default App;
