4c79d64ce321f7bc9f4f955772614ed357d39e8c
susan-t <susan.thao@outlook.com>
2026-03-07 15:29:52 -0500

    basic website formatting

---
+++ src/Home.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import front from './pixel-components/front-page.png'; 
+// import About from "./About";
+
+const Home = () => {
+
+    const navigate = useNavigate();
+
+     const handleClick = () => {
+        navigate("/about");
+    };
+
+  return (
+    <div className="App">
+      <header className="App-body">
+        {/* <img src={front} className="home" alt="front of notebook homepage" /> */}
+        <MyButton className="to-about" onClick={handleClick} />
+      </header>
+    </div>
+  );
+};
+
+function MyButton({ onClick, className }) {
+  return (
+    <button onClick={onClick} className={className}>
+      Hello
+    </button>
+  );
+}
+
+export default Home;
